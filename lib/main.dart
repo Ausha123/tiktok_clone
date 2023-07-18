8b8ec4a8560aef8f9375289f2871c082fde56b43
Ausha123 <aushasandeepa569@gmail.com>
2023-07-18 12:43:51 +0530

    login screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/authentication/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: Colors.black,
+      ),
+      home: LoginScreen(),
+
+    );
+  }
+}
+
+
